005dad3ea61af957dd9d9452b2013193880f5d0b
Taanveer22 <taanveer469@gmail.com>
2025-06-26 17:16:37 +0600

    alhamdulliah functional math done

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ console.log("conneted js");
 let bankBalance = document.getElementById("bank-balance");
 let donationOutput = document.getElementById("donation-output");
 let donationInput = document.getElementById("donation-input");
-console.log(bankBalance, donationOutput, donationInput);
 
 const btnDonateNow = document.getElementById("btn-donate-now");
 // console.log(btnDonateNow);
@@ -13,14 +12,14 @@ const btnDonateNow = document.getElementById("btn-donate-now");
 // event listenter for button donate now
 // ------------------------------------------------------
 btnDonateNow.addEventListener("click", function () {
-  // console.log('donate now clicked');
+  console.log('donate now btn clicked');
   let bankBalanceNumber = parseFloat(bankBalance.innerText);
   console.log(bankBalanceNumber);
 
   let donationOutputNumber = parseFloat(donationOutput.innerText);
   console.log(donationOutputNumber);
 
-  let donationInputNumber = parseFloat(donationInput.value);
+  let donationInputNumber = parseFloat(donationInput.value);q
   console.log(donationInputNumber);
 
   let donatedMoney = donationInputNumber + donationOutputNumber;
